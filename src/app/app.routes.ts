d70970b5b6a079f059af17c69322243bdd9fbcbd
Khin Nyein Chan <khinnyeinchan2199@gmail.com>
2026-09-16 16:36:31 +0630

    structure project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+
+  {
+    path: 'store/:tenantSlug',
+    loadChildren: () =>
+      import('./features/storefront/storefront.routes').then((m) => m.STOREFRONT_ROUTES),
+  },
+
+  {
+    path: 'admin',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/admin/admin.routes').then((m) => m.ADMIN_ROUTES),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'auth/login',
+  },
+];
